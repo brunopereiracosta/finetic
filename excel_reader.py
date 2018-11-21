cd2793d108978c9d346c475ae6584a2a3c30733b
Bruno Pereira Costa <bruno.pereira.costa@tecnico.ulisboa.pt>
2018-11-21 12:55:06 +0000

    Added variable with sheet's mapping and another with  sheets' names.

--- excel_reader.py
+++ excel_reader.py
@@ -3,8 +3,11 @@ file_id = '1atn_R-iKdet3cxx5WCuNavwdHq82qgqG'
 link = 'https://drive.google.com/uc?export=download&id={FILE_ID}'
 csv_url=link.format(FILE_ID=file_id)
 xls = pd.ExcelFile(csv_url)
-df1 = pd.read_excel(xls, 0)
-df2 = pd.read_excel(xls, 1)
+sheet_map = pd.read_excel(xls, sheet_name=None)
+sheet_names = xls.sheet_names
+sheet1 = xls.parse(0)
+#df1 = pd.read_excel(xls, 0)
+#df2 = pd.read_excel(xls, 1)
 #Note that the sheet_name argument to pd.read_excel() can be the name of the sheet
 #(as above), an integer specifying the sheet number (eg 0, 1, etc), a list of
 #sheet names or indices, or None. If a list is provided, it returns a dictionary
@@ -12,4 +15,6 @@ df2 = pd.read_excel(xls, 1)
 #The default is to simply return the first sheet (ie, sheet_name=0).
 #If None is specified, all sheets are returned, as a {sheet_name:dataframe} dictionary.
 
-print(df1)
+#print(sheet_map)
+print(sheet1)
+print(sheet_names)
