535e7ad6e0d5f5c04a318286a72c5ab29e8d2f08
ahw627 <t.e.franca@qmul.ac.uk>
2018-11-27 16:49:45 +0000

    Figures added

--- fit:size_vs_gen.py
+++ fit:size_vs_gen.py
@@ -3,11 +3,13 @@ from MyFuncs import *
 from GA_2 import *
 # from CMAES import *
 
-popu = 250
-ngen = 1000
+popu = 50
+ngen = 180
+# ngen = 100
 # popu = pop_GA/2
-# logbook = run(cxpb=0.11834723511214429, mutpb=0.36282571305589495, n=int(round(0.002018115516200153*L)), tour=int(round(0.12397716233735832*popu)), termpb=0.15381049330039887,popu=popu,ngen=100)
-logbook = run(cxpb=0.5, mutpb=0.25, n=31, tour=10, termpb=0.1,popu=popu,ngen=ngen)
+# logbook = run(cxpb=0.11834723511214429, mutpb=0.36282571305589495, n=int(round(0.002018115516200153*L)), tour=int(round(0.12397716233735832*popu)), termpb=0.15381049330039887,popu=popu,ngen=ngen)
+logbook = run(cxpb=0.29, mutpb=0.29, n=56, tour=19, termpb=0.35,popu=popu,ngen=ngen)
+# logbook = run(cxpb=0.5, mutpb=0.25, n=31, tour=10, termpb=0.1,popu=popu,ngen=ngen)
 
 gen = logbook.select("gen")
 fit_mins = logbook.chapters["fitness"].select("max")
@@ -33,4 +35,4 @@ labs = [l.get_label() for l in lns]
 ax1.legend(lns, labs, loc="center right")
 
 # plt.show()
-plt.savefig("fig_250_1000_2.png")
+plt.savefig("fig_{popu}_{ngen}.png".format(popu=popu,ngen=ngen))
